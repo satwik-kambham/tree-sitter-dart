40225f6a1cac5d1db255b4bd57a224e0148ddd37
Satwik Kambham <satwik.kambham@gmail.com>
2025-04-29 17:40:24 +0530

    updated tree sitter version

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_dart::language()).expect("Error loading dart grammar");
+//! parser.set_language(&tree_sitter_dart::language()).expect("Error loading dart grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
